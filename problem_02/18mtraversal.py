7236234a8ab4e248a2bb4702ef4abb9522947300
Derek Redmond <derekdredmond@gmail.com>
2023-10-04 09:29:44 -0600

    updated traversal for problem 2

--- problem_02/18mtraversal.py
+++ problem_02/18mtraversal.py
@@ -7,8 +7,7 @@ theta_t+1 = theta_t + (v/L) tan(alpha) * delta t
 """
 import matplotlib
 import matplotlib.pyplot as plt
-import numpy
-
+import numpy as np
 
 def main():
     alpha = 0
@@ -20,27 +19,42 @@ def main():
     theta = 0 
     R = 59.0551
     deltaT = .5
-    mark = 0
-    circumfranceT = 46.5
+    time = 0
+    circumfranceT = 46.5 * 2
     trajectory = []
-
-    while mark < circumfranceT:
-        if mark < 2:
-            alpha = 0
-        else: 
-            alpha = numpy.arctan(L/R)
-        x =  x + -v * numpy.sin(theta) * deltaT
-        y = y + v * numpy.cos(theta) * deltaT
-        theta = theta + (v/L) * numpy.tan(alpha) * deltaT
+    
+    while time < circumfranceT:
         trajectory.append((x,y))
-        mark = mark + deltaT
+        if time < 3.25:
+            alpha = np.arctan(2* L/R)
+        else: 
+            alpha = np.arctan(L/R)
+        x =  x + v * np.cos(theta) * deltaT
+        y = y + v * np.sin(theta) * deltaT
+        theta = theta + (v/L) * np.tan(alpha) * deltaT
+        time = time + deltaT
     
     fig,ax = plt.subplots()
     xvals, yvals = zip(*trajectory)
     ax.plot(xvals,yvals, color="blue")
-    matplotlib.patches.Circle((0,0), R, color="green")
+    create_circle(ax, R)
+    window_limit = 70
+    ax.set_xlim(left=-window_limit, right=window_limit)
+    ax.set_ylim(bottom=-window_limit, top=window_limit)
+    ax.set_aspect('equal', adjustable='box')
     plt.show()
 
+def create_circle(ax, R):
+    theta = 0
+    x_vals = []
+    y_vals = []
+    while theta < 2 * np.pi + 0.1:
+        y = R * np.sin(theta)
+        x = R * np.cos(theta)
+        theta = theta + 0.1
+        x_vals.append(x)
+        y_vals.append(y)
+    ax.plot(x_vals,y_vals,color="green")
 
 if __name__ == "__main__":
     main()
