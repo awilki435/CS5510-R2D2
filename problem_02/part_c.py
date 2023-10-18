e37977a3d9248e06f3a80b18220321f6cd323436
Derek Redmond <derekdredmond@gmail.com>
2023-10-18 11:36:02 -0600

    worked on part c

--- problem_02/part_c.py
+++ problem_02/part_c.py
@@ -7,40 +7,35 @@ def main():
     v_l = 0
     L = 35
     W = 10
-    x = R
-    y = 0
-    theta = 0 
+    x_eval = 0
+    y_eval = -R
+    theta_eval = 0 
     deltaT = .1
     time = 0
     circumfranceT = 30
     trajectory = []
 
-    while time < circumfranceT:
-        if 0 < x < 2*R and time < 0.5 * circumfranceT:
-            v_r = 20
-            v_l = (R-W)/(R+W) * v_r
-        else:
-            v_r = 20
-            v_l = (R-W/2)/(R+W/2) * v_r
-        trajectory.append((x,y))
-        x, y, theta = kinematics(W, v_l, v_r, x, y, theta)
-        time = time + deltaT
-    
-    fig,ax = plt.subplots()
-    xvals, yvals = zip(*trajectory)
-    ax.plot(xvals,yvals, color="blue")
-    path_x, path_y = create_circle(ax, R)
-    window_limit = 70
-    ax.set_xlim(left=-window_limit, right=window_limit)
-    ax.set_ylim(bottom=-window_limit, top=window_limit)
-    ax.set_aspect('equal', adjustable='box')
+    T = [1, 0.1, 0.01]
+
+    for t in T:
+        fig, ax = plt.subplots()
+        i = 0 
+        time = []
+        while i < 100*t:
+            x_eval = x_eval - 0.5*t*(v_r + v_l) * np.sin(theta_eval)
+            y_eval = y_eval + 0.5*t*(v_r + v_l) * np.cos(theta_eval)
+            theta_eval = theta_eval + t/W * (v_r - v_l)
+            # assumming constant input
+            v_l = v_l + t
+            v_r = v_r + t
+            i += 1
+
+        window_limit = R+2
+        ax.set_xlim(left=R-window_limit, right=R+window_limit)
+        ax.set_ylim(bottom=-window_limit, top=window_limit)
+        ax.set_aspect('equal', adjustable='box')
     plt.show()
 
-def kinematics(W, v_l, v_r, x0, y0, theta0, dt=0.1):
-    x = x0 + 0.5*(v_l + v_r) * np.cos(theta0) *dt
-    y = y0 + 0.5*(v_l + v_r) * np.sin(theta0) *dt
-    theta = theta0 + 1/W * (v_r - v_l) * dt
-    return x,y,theta
 
 def create_circle(ax, R):
     theta = 0
@@ -48,7 +43,7 @@ def create_circle(ax, R):
     y_vals = []
     while theta < 2 * np.pi + 0.1:
         y = R * np.sin(theta)
-        x = R * np.cos(theta)
+        x = R * np.cos(theta) + R
         theta = theta + 0.1
         x_vals.append(x)
         y_vals.append(y)
