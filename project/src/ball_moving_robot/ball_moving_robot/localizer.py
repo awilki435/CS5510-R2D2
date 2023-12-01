307816256aac33bc886d2946bf1e5a16792b8953
Andrew Wilkinson <andrew.wilkinson@usu.edu>
2023-12-01 13:27:46 -0700

    Ball and goal finder

--- project/src/ball_moving_robot/ball_moving_robot/localizer.py
+++ project/src/ball_moving_robot/ball_moving_robot/localizer.py
@@ -5,6 +5,8 @@
 # send a PoseStamped message to the planner
 
 import typing
+import cv2
+import numpy as np
 import rclpy
 from rclpy.node import Node
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
@@ -59,19 +61,73 @@ class Localizer(Node):
 
     def find_ball(self) -> PoseStamped:
         """Find where the ball is."""
+        # Read the captured image
+        # TODO: Change the image to an image captured by the robot
+        image = cv2.imread('ball_moving_robot/balls.jpg')
+
+        # Convert the image to the HSV color space
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         self.get_logger().info("Finding ball...")
+        lower_red = np.array([0, 100, 100])
+        upper_red = np.array([10, 255, 255])
+        mask_red = cv2.inRange(hsv, lower_red, upper_red)
+        contours, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        largest_contour = max(contours, key=cv2.contourArea)
+
+        # Calculate the centroid of the largest contour
+        M = cv2.moments(largest_contour)
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+            #Testing:
+            # Print or use the x, y coordinates of the center centroid
+            #print(f"Center Centroid: x={cx}, y={cy}")
+
+            # Optionally, draw a circle at the center centroid on the original image
+            #cv2.circle(image, (cx, cy), 5, (0, 255, 0), -1)
+        # Display the original image with centroids marked
+        #cv2.namedWindow('Original Image with Centroids', cv2.WINDOW_NORMAL)
+        #cv2.imshow('Original Image with Centroids', image)
         msg = PoseStamped()
-        msg.pose.position.x = 0.0
-        msg.pose.position.y = 1.0
+        msg.pose.position.x = cx
+        msg.pose.position.y = cy
         msg.header.frame_id = "map"
         self.goal_pub.publish(msg)
 
     def find_goal(self) -> PoseStamped:
         """Find where the goal is."""
+        # Read the captured image
+        # TODO: Change the image to each image captured by the robot
+        image = cv2.imread('ball_moving_robot/balls.jpg')
+
+        # Convert the image to the HSV color space
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        self.get_logger().info("Finding ball...")
+        lower_green = np.array([40, 40, 40])
+        upper_green = np.array([80, 255, 255])
+        mask_green = cv2.inRange(hsv, lower_green, upper_green)
+        contours, _ = cv2.findContours(mask_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        largest_contour = max(contours, key=cv2.contourArea)
+
+        # Calculate the centroid of the largest contour
+        M = cv2.moments(largest_contour)
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+            #Testing:
+            # Print or use the x, y coordinates of the center centroid
+            #print(f"Center Centroid: x={cx}, y={cy}")
+
+            # Optionally, draw a circle at the center centroid on the original image
+            #cv2.circle(image, (cx, cy), 5, (0, 255, 0), -1)
+        #Testing: 
+        # Display the original image with centroids marked
+        #cv2.namedWindow('Original Image with Centroids', cv2.WINDOW_NORMAL)
+        #cv2.imshow('Original Image with Centroids', image)
         self.get_logger().info("Finding goal...")
         msg = PoseStamped()
-        msg.pose.position.x = 5.0
-        msg.pose.position.y = 0.0
+        msg.pose.position.x = cx
+        msg.pose.position.y = cy
         msg.header.frame_id = "map"
         self.goal_pub.publish(msg)
 
