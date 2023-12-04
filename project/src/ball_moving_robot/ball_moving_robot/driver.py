e20ac8fbe47d20f43b3e148630e562f82596e3a1
Mark Soulier <marksoulkid@icloud.com>
2023-12-04 16:17:29 -0700

    driver

--- project/src/ball_moving_robot/ball_moving_robot/driver.py
+++ project/src/ball_moving_robot/ball_moving_robot/driver.py
@@ -2,6 +2,7 @@
 
 # import smbus
 import math
+import numpy as np
 import rclpy
 from rclpy.node import Node
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
@@ -20,13 +21,17 @@ class Driver(Node):
     def __init__(self):
         """Initialize Node."""
         super().__init__(node_name="Driver")
-        self.get_logger().info("Initializing...")
+        self.get_logger().info("Initializing Driver...")
 
         # variables to move the robot
         self._addr = 0x16
         # self._device = smbus.SMBus(1)
         self.v = 0.0
         self.omega = 0.0
+
+        #define vehicle parameters
+        self.wheel_radius = 0.05
+        self.axle_length = 0.1
 
         # Create callback groups
         self.cb_move = MutuallyExclusiveCallbackGroup()
@@ -48,7 +53,7 @@ class Driver(Node):
 
     def velocity_callback(self, msg: Twist):
         """Receive velocity commands."""
-        self.v = msg.linear.y
+        self.v = msg.linear.x
         self.omega = msg.angular.z
 
     def move_robot(self):
@@ -59,33 +64,38 @@ class Driver(Node):
         sets the motor with the speed given (not actually in unit, just a power amount)
         """
 
-        # # Convert to Differential Drive:
-        # inputs = np.array([[v], [omega]])
-        # r = self.wheel_radius
-        # L = self.axle_length
-        # transform = np.linalg.inv(np.array([[r / 2, r / 2], [r / L, -r / L]]))
-        # right, left = transform @ inputs
+        # Convert to Differential Drive:
+        inputs = np.array([[self.v], [self.omega]])
+        r = self.wheel_radius
+        L = self.axle_length
+        transform = np.linalg.inv(np.array([[r / 2, r / 2], [r / L, -r / L]]))
+        right, left = transform @ inputs
 
-        # self.v_r = float(right)
-        # self.v_l = float(left)
+        # Scale magnitude to be between -255 and 255
+        max_speed = max(abs(right), abs(left))
+        if max_speed > 255:
+            right = right / max_speed * 255
+            left = left / max_speed * 255
 
-        # TODO: Unit stuff
+        self.v_r = float(right)
+        self.v_l = float(left)
+        
+        # Controls to the actual robot
+        register = 0x01
+        left_direction = 0 if left < 0 else 1
+        right_direction = 0 if right < 0 else 1
 
-        # # Controls to the actual robot
-        # register = 0x01
-        # left_direction = 0 if left < 0 else 1
-        # right_direction = 0 if right < 0 else 1
+        if left < 0:
+            left *= -1
+        if right < 0:
+            right *= -1
 
-        # if left < 0:
-        #     left *= -1
-        # if right < 0:
-        #     right *= -1
-
-        # data = [left_direction, left, right_direction, right]
-        # try:
-        #     self._device.write_i2c_block_data(self._addr, register, data)
-        # except:
-        #     self.get_logger().error("write array error in control_car")
+        data = [left_direction, left, right_direction, right]
+        try:
+            # smbus.SMBus(1).write_i2c_block_data(self._addr, register, data)
+            print("left: ", left, "right: ", right)
+        except:
+            self.get_logger().error("write array error in control_car")
 
 
 def main(args=None):
